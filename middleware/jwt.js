c8fa3dc2a046be9253d2da20e91fd9d840181346
aka2910 <200050004@iitb.ac.in>
2021-10-22 23:10:05 +0530

    added download all submission and reformated completed directory

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,34 +1,33 @@
-
-const jwt  = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
-const User = require('../models/user')
+const User = require("../models/user");
 
 async function jwt_verify(req, res, next) {
-    const exceptions = ['/', '/login', '/register'];
-    if(exceptions.includes(req.path)){
-        return next();
-    }
-    try{
-        var token = req.headers.cookie;
-
-        if(!token){
-            return res.redirect("/login");
-        }
-
-        token = token.split("jwt=")[1];
-        await jwt.verify(token, JWT_SECRET, async (err, verifiedJwt) => {
-            if(err){
-                return res.redirect("/login");
-            }else{
-                const username = verifiedJwt["username"];
-                req.user = await User.findOne({username});
-            }
-        })
-    }catch(e){
-        console.log(e);
-        return res.redirect("/login");
-    }
+  const exceptions = ["/", "/login", "/register"];
+  if (exceptions.includes(req.path)) {
     return next();
-};
+  }
+  try {
+    var token = req.headers.cookie;
 
-module.exports = jwt_verify
+    if (!token) {
+      return res.redirect("/login");
+    }
+
+    token = token.split("jwt=")[1];
+    await jwt.verify(token, JWT_SECRET, async (err, verifiedJwt) => {
+      if (err) {
+        return res.redirect("/login");
+      } else {
+        const username = verifiedJwt["username"];
+        req.user = await User.findOne({ username });
+      }
+    });
+  } catch (e) {
+    console.log(e);
+    return res.redirect("/login");
+  }
+  return next();
+}
+
+module.exports = jwt_verify;
